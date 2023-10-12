ddc694461a9e7f0469e513cefaf88dcfa78633ad
CosminManu <cosmin.manu2000@gmail.com>
2023-10-12 12:23:23 +0300

    finished customization for login form; made window non resizable; started the application

--- src/main/java/com/cosminmanu/ebanking/controllers/LoginController.java
+++ src/main/java/com/cosminmanu/ebanking/controllers/LoginController.java
@@ -1,4 +1,16 @@
 package com.cosminmanu.ebanking.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox cb_acc_selector;
+    public Label lbl_payee_address;
+    public TextField textf_payee_address;
+    public Label lbl_password;
+    public TextField textf_password;
+    public Button btn_login;
+    public Label lbl_error;
 }
